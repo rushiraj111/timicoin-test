59fec436652bdf94a96c3e4a468d243ab9906e5c
Rushirajsinh Jadeja <rushiraj111@gmail.com>
2018-09-04 01:59:51 +0530

    with reference to copyright violation issue closes #1

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 // Copyright (c) 2017-2018 The PIVX Developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// asdfasdfasdf
 
 #include "main.h"
 
